fb94ccf8998ac895b6808b7fe349dabc13247b11
Alexandre GUYOT <a.pro.guyot@gmail.com>
2019-01-21 19:28:36 +0100

    Ajout des utilisateurs

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,7 @@
 use Illuminate\Http\Request;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+    Route for backend
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
