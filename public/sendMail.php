574bcb828a21b2179e14ee952c744e7c0badc93c
Guillaume Sourisseau <sourisseauguillaume@gmail.com>
2019-02-06 19:30:01 +0100

    MAJ sendMail.php

--- public/sendMail.php
+++ public/sendMail.php
@@ -1,26 +1,18 @@
 <?php
 
-
-  $message = sprintf("Nom du contact : %s - Email : %s\n\nSujet : %s\n\n Information désirée  : %s\n\n Societe  : %s\n\n Message : %s\n", $_POST['name'], $_POST['surname'], $_POST['email'], $_POST['telephone'], $_POST['message']);
+  $message = sprintf("Nom du contact : %s %s - Email : %s\n\n Téléphone : %s\n\n Message : %s\n", $_POST['name'], $_POST['surname'], $_POST['email'], $_POST['telephone'], $_POST['message']);
 
     $headers = 'From: sourisseauguillaume@gmail.com' . "\r\n" .
          'X-Mailer: PHP/' . phpversion();
 
     mail('sourisseauguillaume@gmail.com', 'E-mail reçu via votre site internet www.imaa.eu', $message, $headers);
-
     Redirect('http://www.preprod.imaa.eu', false);
 
+	function Redirect($url, $permanent = false)
+	{
+	    header('Location: ' . $url, true, $permanent ? 301 : 302);
 
-
-function Redirect($url, $permanent = false)
-{
-    header('Location: ' . $url, true, $permanent ? 301 : 302);
-
-    exit();
-}
-
-
-
-
+	    exit();
+	}
 
 ?>
