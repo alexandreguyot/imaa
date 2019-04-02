474cef4c69ce832ffb0e505b38cef18bf4aa4f8e
Guillaume Sourisseau <sourisseauguillaume@gmail.com>
2019-04-03 00:26:50 +0200

    traitement des retours suite rdv 0104

--- resources/views/contents/erp/projects.blade.php
+++ resources/views/contents/erp/projects.blade.php
@@ -43,11 +43,6 @@
 								<div class="dashboard">
 									<div class="banner">
 										<div id="carouselExampleIndicators-1" class="carousel slide" data-ride="carousel">
-											<ol class="carousel-indicators">
-												<li data-target="#carouselExampleIndicators-1" data-slide-to="0" class="active"></li>
-												<li data-target="#carouselExampleIndicators-1" data-slide-to="1"></li>
-												<li data-target="#carouselExampleIndicators-1" data-slide-to="2"></li>
-											</ol>
 											<div class="carousel-inner">
 												@if($dashboard->photos)
 													@foreach(explode(';', $dashboard->photos[0]) as $photo)
